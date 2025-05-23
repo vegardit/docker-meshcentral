5c0026b42d2342f97af2c077ca74b26d2ec7edf0
sebthom <sebthom@users.noreply.github.com>
2025-05-23 12:27:02 +0200

    ci: improve multi-arch builds

--- build-image.sh
+++ build-image.sh
@@ -14,32 +14,67 @@ shared_lib="$(dirname "${BASH_SOURCE[0]}")/.shared"
 # shellcheck disable=SC1091  # Not following: $shared_lib/lib/build-image-init.sh was not specified as input
 source "$shared_lib/lib/build-image-init.sh"
 
+if [[ $OSTYPE == "cygwin" || $OSTYPE == "msys" ]]; then
+   project_root=$(cygpath -w "$project_root")
+fi
 
 #################################################
 # specify target repo and image name
 #################################################
 image_repo=${DOCKER_IMAGE_REPO:-vegardit/meshcentral}
 base_image_name=${DOCKER_BASE_IMAGE:-node:lts-slim}
-image_name=$image_repo:latest
+image_name=$image_repo:${DOCKER_IMAGE_TAG:-latest}
 
 
 #################################################
-# build the image
+# define target plaforms for multiach builds
 #################################################
-log INFO "Building docker image [$image_name]..."
-if [[ $OSTYPE == "cygwin" || $OSTYPE == "msys" ]]; then
-   project_root=$(cygpath -w "$project_root")
+if [[ ${DOCKER_PUSH:-} == "true" || ${DOCKER_PUSH_GHCR:-} == "true" ]]; then
+   platforms="linux/amd64,linux/arm64/v8" #linux/arm/v7
+   build_multi_arch="true"
 fi
 
+
+#################################################
+# prepare docker
+#################################################
+(set -x; docker version)
+
 # https://github.com/docker/buildx/#building-multi-platform-images
-set -x
+(set -x; docker buildx version) # ensures buildx is enabled
 
-docker --version
 export DOCKER_BUILDKIT=1
-export DOCKER_CLI_EXPERIMENTAL=1 # prevents "docker: 'buildx' is not a docker command."
+export DOCKER_CLI_EXPERIMENTAL=1 # prevents "docker: 'buildx' is not a docker command." in older Docker versions
 
-# Register QEMU emulators for all architectures so Docker can run and build multi-arch images
-docker run --privileged --rm ghcr.io/dockerhub-mirror/tonistiigi__binfmt --install all
+if [[ ${build_multi_arch:-} == "true" ]]; then
+
+   # Use a temporary local registry to work around Docker/Buildx/BuildKit quirks,
+   # enabling us to build/test multiarch images locally before pushing.
+   for local_registry_port in {5000..6000}; do
+      if ! lsof -iTCP:"$local_registry_port" -sTCP:LISTEN -t >/dev/null; then
+         local_registry_container_name="local-registry-$local_registry_port"
+         (set -x; docker run -d --rm -p "$local_registry_port:5000" \
+            --name "$local_registry_container_name" \
+            ghcr.io/dockerhub-mirror/registry)
+         local_registry="127.0.0.1:$local_registry_port"
+         trap 'docker stop "$local_registry_container_name"' EXIT
+
+         log INFO "Waiting for Docker registry at [$local_registry] to be ready..."
+         until curl -sSf "http://$local_registry/v2/"; do sleep 0.1; done
+         log INFO "✅ Registry is ready"
+         break
+      fi
+   done
+   if [[ -z "${local_registry:-}" ]]; then
+      echo "❌ No free TCP port between 5000–6000" >&2
+      exit 1
+   fi
+fi
+
+if [[ ${build_multi_arch:-} == "true" ]]; then
+   # Register QEMU emulators so Docker can run and build multi-arch images
+   (set -x; docker run --privileged --rm ghcr.io/dockerhub-mirror/tonistiigi__binfmt --install all)
+fi
 
 # https://docs.docker.com/build/buildkit/configure/#resource-limiting
 echo "
@@ -47,59 +82,67 @@ echo "
   max-parallelism = 3
 " | sudo tee /etc/buildkitd.toml
 
-docker buildx version # ensures buildx is enabled
-docker buildx create --config /etc/buildkitd.toml --use # prevents: error: multiple platforms feature is currently not supported for docker driver. Please switch to a different driver (eg. "docker buildx create --use")
-trap 'docker buildx stop' EXIT
-# shellcheck disable=SC2154,SC2046  # base_layer_cache_key is referenced but not assigned / Quote this to prevent word splitting
-docker buildx build "$project_root" \
-   --file "image/Dockerfile" \
-   --progress=plain \
-   --pull \
-   --build-arg INSTALL_SUPPORT_TOOLS="${INSTALL_SUPPORT_TOOLS:-0}" \
-   `# using the current date as value for BASE_LAYER_CACHE_KEY, i.e. the base layer cache (that holds system packages with security updates) will be invalidate once per day` \
-   --build-arg BASE_LAYER_CACHE_KEY="$base_layer_cache_key" \
-   --build-arg BASE_IMAGE="$base_image_name" \
-   --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-   --build-arg GIT_BRANCH="${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}" \
-   --build-arg GIT_COMMIT_DATE="$(date -d "@$(git log -1 --format='%at')" --utc +'%Y-%m-%d %H:%M:%S UTC')" \
-   --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
-   --build-arg GIT_REPO_URL="$(git config --get remote.origin.url)" \
-   $(if [[ ${ACT:-} == "true" || ${DOCKER_PUSH:-} != "true" ]]; then \
-      echo -n "--load --output type=docker"; \
-   else \
-      echo -n "--platform linux/amd64,linux/arm64" `# ,linux/arm/v7"`; \
-   fi) \
-   --tag "$image_name" \
-   $(if [[ ${DOCKER_PUSH:-} == "true" ]]; then echo -n "--push"; fi) \
-   "$@"
-set +x
+builder_name="bx-$(date +%s)-$RANDOM"
+(set -x; docker buildx create \
+   --name "$builder_name" \
+   --bootstrap \
+   --config /etc/buildkitd.toml \
+   --driver-opt network=host `# required for buildx to access the temporary registry` \
+   --driver docker-container)
+trap 'docker buildx rm --force "$builder_name"' EXIT
 
-if [[ ${DOCKER_PUSH:-} == "true" ]]; then
-   docker image pull "$image_name"
+
+#################################################
+# build the image
+#################################################
+log INFO "Pulling base image [$base_image_name]..."
+if [[ ${build_multi_arch:-} == "true" ]]; then
+   for platform in ${platforms//,/ }; do
+      docker pull --platform "$platform" "$base_image_name"
+   done
+else
+   docker pull "$base_image_name"
 fi
 
-#################################################
-# determine effective MeshCentral version
-#################################################
-# LC_ALL=en_US.utf8 -> workaround for "grep: -P supports only unibyte and UTF-8 locales"
-# 2>/dev/null -> workaround for "write /dev/stdout: The pipe is being closed."
-meshcentral_version=$(docker run --rm "$image_name" node node_modules/meshcentral --help 2>/dev/null | LC_ALL=en_US.utf8 grep -oP 'MeshCentral v\K\d+\.\d+\.\d+' | head -1 || true)
-echo "meshcentral_version=$meshcentral_version"
+log INFO "Building docker image [$image_name]..."
+
+# common build arguments
+build_opts=(
+   --builder "$builder_name"
+   --progress=plain
+   --file "image/Dockerfile"
+   --build-arg INSTALL_SUPPORT_TOOLS="${INSTALL_SUPPORT_TOOLS:-0}"
+   # using the current date as value for BASE_LAYER_CACHE_KEY, i.e. the base layer cache (that holds system packages with security updates) will be invalidate once per day
+   --build-arg BASE_LAYER_CACHE_KEY="$base_layer_cache_key"
+   --build-arg BASE_IMAGE="$base_image_name"
+   --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+   --build-arg GIT_BRANCH="${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
+   --build-arg GIT_COMMIT_DATE="$(date -d "@$(git log -1 --format='%at')" --utc +'%Y-%m-%d %H:%M:%S UTC')"
+   --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)"
+   --build-arg GIT_REPO_URL="$(git config --get remote.origin.url)"
+)
+
+if [[ ${build_multi_arch:-} == "true" ]]; then
+   build_opts+=(--push)
+   build_opts+=(--sbom=true) # https://docs.docker.com/build/metadata/attestations/sbom/#create-sbom-attestations
+   build_opts+=(--platform "$platforms")
+   build_opts+=(--tag "$local_registry/$image_name")
+else
+   build_opts+=(--output "type=docker,load=true")
+   build_opts+=(--tag "$image_name")
+fi
+
+# shellcheck disable=SC2154,SC2046  # base_layer_cache_key is referenced but not assigned / Quote this to prevent word splitting
+(set -x; docker buildx build "${build_opts[@]}" "$project_root")
 
 
 #################################################
-# apply tags
+# load image into local docker daemon for testing
 #################################################
-declare -a tags=()
-tags+=("$image_repo:${meshcentral_version%.*}.x")  # :0.8.x
-tags+=("$image_repo:${meshcentral_version%%.*}.x") # :0.x
-for tag in "${tags[@]}"; do
-   (set -x; docker image tag "$image_name" "$tag")
-   if [[ ${DOCKER_PUSH:-} == "true" ]]; then
-      (set -x; docker push "$tag")
-   fi
-done
-tags+=("$image_name") # :latest
+if [[ ${build_multi_arch:-} == "true" ]]; then
+   docker pull "$local_registry/$image_name"
+   docker tag "$local_registry/$image_name" "$image_name"
+fi
 
 
 #################################################
@@ -111,16 +154,46 @@ fi
 
 
 #################################################
-# push image to ghcr.io
+# determine effective MeshCentral version
 #################################################
-if [[ ${DOCKER_PUSH_GHCR:-} == "true" ]]; then
+echo
+log INFO "Determining effective MeshCentral version of [$image_name]..."
+# LC_ALL=en_US.utf8 -> workaround for "grep: -P supports only unibyte and UTF-8 locales"
+# 2>/dev/null -> workaround for "write /dev/stdout: The pipe is being closed."
+meshcentral_version=$(docker run --rm "$image_name" node node_modules/meshcentral --help 2>/dev/null | LC_ALL=en_US.utf8 grep -oP 'MeshCentral v\K\d+\.\d+\.\d+' | head -1 || true)
+echo "meshcentral_version=$meshcentral_version"
+
+
+#################################################
+# calculate tags
+#################################################
+declare -a tags=()
+tags+=("$image_name") # :latest
+tags+=("$image_repo:${meshcentral_version%.*}.x")  # :0.8.x
+tags+=("$image_repo:${meshcentral_version%%.*}.x") # :0.x
+
+
+#################################################
+# push image
+#################################################
+function regctl() {
+   (set -x;
+   docker run --rm \
+      -u "$(id -u):$(id -g)" -e HOME -v "$HOME:$HOME" \
+      -v /etc/docker/certs.d:/etc/docker/certs.d:ro \
+      --network host `# required to access the temporary registry` \
+      ghcr.io/regclient/regctl:latest \
+      --host "reg=$local_registry,tls=disabled" \
+      "${@}")
+}
+
+if [[ ${DOCKER_PUSH:-} == "true" ]]; then
    for tag in "${tags[@]}"; do
-      set -x
-      docker run --rm \
-         -u "$(id -u):$(id -g)" -e HOME -v "$HOME:$HOME" \
-         -v /etc/docker/certs.d:/etc/docker/certs.d:ro \
-         ghcr.io/regclient/regctl:latest \
-         image copy "$tag" "ghcr.io/$tag"
-      set +x
+      regctl image copy "$local_registry/$image_name" "docker.io/$tag"
+   done
+fi
+if [[ ${DOCKER_PUSH_GHCR:-} == true ]]; then
+   for tag in "${tags[@]}"; do
+      regctl image copy "$local_registry/$image_name" "ghcr.io/$tag"
    done
 fi
